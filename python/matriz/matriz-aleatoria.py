8d4c142c4c82ef2977575b7d9fa14bd06595505f
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2024-06-29 15:37:21 -0300

    added matriz-aleatoria :rocket:

---
+++ python/matriz/matriz-aleatoria.py
@@ -0,0 +1,15 @@
+import random
+
+while True:
+    lin = int(input("Quantidade de linhas da matriz: "))
+    col = int(input("Quantidade de colunas da matriz: "))
+    if lin > 0 and col > 0:
+        M = []
+        for i in range(lin):
+            LINHA = []
+            for j in range(col):
+                num = random.randint(1, 15)
+                LINHA.append(num)
+            M.append(LINHA)
+        break
+print(M)
