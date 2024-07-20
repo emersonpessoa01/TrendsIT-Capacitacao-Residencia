81b938bb02a4fbe7fb0172498218efb64614facb
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2024-07-19 21:37:31 -0300

    Java course started :rocket:

---
+++ Curso_de_Java/src/poo/Carro.java
@@ -0,0 +1,19 @@
+package poo;
+
+public class Carro {
+	int ano;
+	String cor;
+
+	void ligar() {
+		System.out.println("Engine ON...");
+	}
+
+	void desligar() {
+		System.out.println("Engine OFF...");
+	}
+
+	void acelerar() {
+		System.out.println("Vrummmmmmm...");
+	}
+
+}
