c5549ce4dd996faa894bf32c4f7ba83709f85d4a
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2024-07-20 17:07:06 -0300

    added Class Carro, Uno, Aviao and Cessna :rocket:

--- Curso_de_Java/src/poo/Uno.java
+++ Curso_de_Java/src/poo/Uno.java
@@ -1,6 +1,10 @@
 package poo;
 
 public class Uno {
+<<<<<<< HEAD
+=======
+
+>>>>>>> 751acaa1614533afc97527b40bb73c8bfb16f8db
 	public static void main(String[] args) {
 		Carro uno = new Carro(1991, "Branco");
 		System.out.println("Carro: Uno");
