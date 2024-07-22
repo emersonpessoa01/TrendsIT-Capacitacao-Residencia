d71d7055c45d6d41d586ed19cdcbd42cfd92ba86
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2024-07-21 21:41:04 -0300

    Studying main, static and main methods :tada:

--- Curso_de_Java/src/fundamentos/AgenciaBancaria/contas/Conta.java
+++ Curso_de_Java/src/fundamentos/AgenciaBancaria/contas/Conta.java
@@ -1,9 +1,10 @@
 package fundamentos.AgenciaBancaria.contas;
 
+//É uma classe modelo
 //Exemplo de modificador de acesso Public
 //Exemplo de modificador de acesso Private
 public class Conta {
-	//Public
+	// Public
 	// atributos
 //	public String cliente;
 //	public double saldo;
@@ -18,19 +19,46 @@ public class Conta {
 //		System.out.println("Saldo: R$ " + saldo);
 //
 //	}
-	//Private
+	// Private
 	// atributos
-		protected String cliente;
-		protected double saldo;
+	protected String cliente;
+	protected double saldo;
 
-		// construtor
-		public Conta() {
-			System.out.println("Agência 0261");
+	// construtor
+	public Conta() {
+		System.out.println("Agência 0261");
 
-		}
+	}
 
-		protected void exibirSaldo() {
-			System.out.println("Saldo: R$ " + saldo);
+	protected void exibirSaldo() {
+		System.out.println("Saldo: R$ " + saldo);
 
-		}
+	}
+
+	// Métodos com passagem de parâmetros
+	void sacar(double valor) {
+		saldo -= valor;
+		System.out.println("Débito: R$ " + valor);
+	}
+
+	void depositar(double valor) {
+		saldo += valor;
+		System.out.println("Crédito: R$ " + valor);
+	}
+
+	// Métodoscom passagem de objetos
+	void transferir(Conta destino, double valor) {
+		this.sacar(valor);
+		destino.depositar(valor);
+		System.out.println("Transferência: R$ " + valor);
+	}
+
+	// Método com retornos
+	// Emissão de relatório de saldos
+	double soma(double cc1, double cc2) {
+		double total = cc1 + cc2;
+		return total;
+		
+	}
+
 }
--- Curso_de_Java/src/fundamentos/AgenciaBancaria/contas/PessoaFisica.java
+++ Curso_de_Java/src/fundamentos/AgenciaBancaria/contas/PessoaFisica.java
@@ -21,17 +21,40 @@ public class PessoaFisica {
 		//Protected
 		Conta cc1 = new Conta();
 		cc1.cliente = "Emerson Pessoa";
-		cc1.saldo = 100000;
+		cc1.saldo = 10000;
 		System.out.println("Cliente: "+ cc1.cliente);
+		cc1.exibirSaldo();
+		cc1.sacar(1000);
 		cc1.exibirSaldo();
 		
 		System.out.println("-----------------------------------");
 		
 		Conta cc2 = new Conta();
 		cc2.cliente = "Luciene Ferreira";
-		cc2.saldo = 200000;
+		cc2.saldo = 20000;
 		System.out.println("Cliente: "+ cc2.cliente);
 		cc2.exibirSaldo();
+		cc2.depositar(1000);
+		cc2.exibirSaldo();
+		
+		System.out.println("-----------------------------------");
+		System.out.println("Transferência: ");
+		System.out.println("Cliente: "+ cc1.cliente);
+		System.out.println("Favorecido: "+ cc2.cliente);
+		cc1.transferir(cc2, 2000);
+		System.out.println("");
+		System.out.println("Cliente: "+ cc1.cliente);
+		cc1.exibirSaldo();
+		System.out.println("Cliente: "+ cc2.cliente);
+		cc2.exibirSaldo();
+		System.out.println("-----------------------------------");
+		System.out.println("Relatório gerencial: ");
+		Conta gerente= new Conta();
+		double relatorio = gerente.soma(cc1.saldo, cc2.saldo);
+		System.out.println("Saldo total nas contas: R$ "+ relatorio);
+		
+		
+		
 		
 		
 	}
