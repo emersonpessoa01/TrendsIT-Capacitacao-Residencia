d8a31ae291483a941f718bdb11763887aae40cb2
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2024-07-23 13:16:36 -0300

    added Regra de Três :rocket:

---
+++ Curso_de_Java/src/fundamentos/exercicios/RegraDeTres2.java
@@ -0,0 +1,31 @@
+package fundamentos.exercicios;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class RegraDeTres2 {
+
+	public static void main(String[] args) {
+		// variaveis
+		double x, y, valor;
+		// objetos
+		Scanner teclado = new Scanner(System.in);
+		DecimalFormat formatador = new DecimalFormat("#0.00");
+		// Entrada
+		System.out.println("Regra de três");
+		System.out.println("x% de y = valor");
+		System.out.println("Digite o valor de x: ");
+		x = teclado.nextDouble();
+		System.out.println("Digite o valor de y: ");
+		y = teclado.nextDouble();
+
+		// processamento
+		valor = (x * y) / 100;
+
+		// saida
+		System.out.println(x + "% de " + y + " = " +  formatador.format(valor));
+		teclado.close();
+
+	}
+
+}
