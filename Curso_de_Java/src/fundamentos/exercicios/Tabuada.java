1fc1a0bf3b75f93b700dd374f5e7cae4e2c6e7b8
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2024-07-27 15:36:29 -0300

    Added tabuada :rocket:

---
+++ Curso_de_Java/src/fundamentos/exercicios/Tabuada.java
@@ -0,0 +1,27 @@
+package fundamentos.exercicios;
+/*
+ * Curso de Java aula 9 - estrutura de repetição
+ * @autor José de Assis
+ */
+
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Tabuada {
+
+	public static void main(String[] args) {
+		int valor;
+
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Digite o valor a tabuada: ");
+		valor = teclado.nextInt();
+		System.out.println("");
+
+		for (int i = 0; i <= 10; i++) {
+			System.out.println(valor + "x" + i + " = " + (valor * i));
+			
+
+		}
+	}
+
+}
