abdbee29470e7e854773f2f953ef748c3b7377f6
emersonpessoa01 <eps011108@gmail.com>
2024-12-27 20:33:56 -0300

    feat: C# classes started :tada:

---
+++ 02.Nivelamento Prático/Curso de C#/repos/pooMensagem/pooMensagem/Mensagem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooMensagem
+{
+    public class Mensagem
+    {
+       public String TextoMensagem;
+    
+      public void ExibirMensagem()
+        {
+            Console.WriteLine(this.TextoMensagem);
+
+        }
+    }
+}
---
+++ 02.Nivelamento Prático/Curso de C#/repos/pooMensagem/pooMensagem/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooMensagem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Olá mundo!");
+            Mensagem msg1, msg2;
+            msg1 = new Mensagem();
+            msg1.TextoMensagem = "Olá mundo!";
+            //Console.WriteLine(msg1.TextoMensagem);
+            msg1.ExibirMensagem();
+
+            msg2  = new Mensagem();
+            msg2.TextoMensagem = "Segundo objeto";
+            msg2.ExibirMensagem();
+            Console.ReadKey();
+        }
+    }
+}
