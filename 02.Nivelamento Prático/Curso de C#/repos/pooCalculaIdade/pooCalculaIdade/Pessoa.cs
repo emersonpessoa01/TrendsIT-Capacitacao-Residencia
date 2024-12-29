a26ab229add22fd17338d157ea65084b78af2b7b
emersonpessoa01 <eps011108@gmail.com>
2024-12-29 20:19:29 -0300

    feat: Added pooCalculaIdade :tada:

---
+++ 02.Nivelamento Prático/Curso de C#/repos/pooCalculaIdade/pooCalculaIdade/Pessoa.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooCalculaIdade
+{
+    public class Pessoa
+
+    //Encapsulamento dos dados
+    {
+        private int anoNascimento;
+
+        public int AnoNascimento
+        {
+            get { return anoNascimento; }
+            set { anoNascimento = value; }
+        }
+
+        private String nome;
+
+        public String Nome
+        {
+            get { return nome; }
+            set { nome = value.ToUpper(); }
+        }
+
+        //Executar uma ação exibindo uma data de nascimento
+
+        public void exibirDados()
+        {
+            Console.WriteLine("Nome: " + this.Nome);
+            Console.WriteLine("Ano de nascimento: " + this.AnoNascimento);
+            int idade = this.CalcularIdade();
+            Console.WriteLine("Idade: "+idade);
+
+        }
+        private int CalcularIdade()
+        {
+            DateTime data = DateTime.Now;
+            int ano = data.Year;
+            int idade = ano - this.anoNascimento;
+            return idade;
+        }
+    }
+}
---
+++ 02.Nivelamento Prático/Curso de C#/repos/pooCalculaIdade/pooCalculaIdade/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooCalculaIdade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que calcula a idade de uma pessoa");
+            Pessoa pessoa = new Pessoa();
+            Console.WriteLine("Nome da pessoa: ");
+            pessoa.Nome = Console.ReadLine();
+            Console.WriteLine("Ano de nascimento");
+            pessoa.AnoNascimento = Convert.ToInt32(Console.ReadLine());
+            pessoa.exibirDados();
+            Console.ReadKey();
+
+        }
+    }
+}
