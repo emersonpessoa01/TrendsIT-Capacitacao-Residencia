74bac65ec67dcce93eaa8637259d2dd47dcd9201
emersonpessoa01 <eps011108@gmail.com>
2024-12-28 21:05:42 -0300

    feat: Added Properties in C# :rocket:

--- 02.Nivelamento Prático/Curso de C#/repos/pooMensagem/pooMensagem/Mensagem.cs
+++ 02.Nivelamento Prático/Curso de C#/repos/pooMensagem/pooMensagem/Mensagem.cs
@@ -8,24 +8,44 @@ namespace pooMensagem
 {
     public class Mensagem
     {
-       private String TextoMensagem;
-    
-      //public void ExibirMensagem()
-      //  {
-      //      Console.WriteLine(this.TextoMensagem);
+        //private String TextoMensagem;
 
-      //  }
+        //public void ExibirMensagem()
+        //  {
+        //      Console.WriteLine(this.TextoMensagem);
+
+        //  }
 
         //Encapsulamento
         //Métodos de acesso da classe 
-        public String getTextoMensagem()
+        //public String getTextoMensagem()
+        //{
+        //    return this.TextoMensagem; // 'this' serve para interagir com algo que é do próprio objeto
+
+        //}
+        //public void setTextoMensagem(String textoMensagem)
+        //{
+        //    this.TextoMensagem = textoMensagem.ToUpper();
+        //}
+
+
+        private String textoMensagem;
+        public String TextoMensagem
         {
-            return this.TextoMensagem; // 'this' serve para interagir com algo que é do próprio objeto
+            get { 
+                return textoMensagem;
+            }
+            set { 
+                textoMensagem = value; 
+            }
+           
+        }
+
+        public void ExibirMensagem()
+        {
+            Console.WriteLine(this.TextoMensagem);
 
         }
-        public void setTextoMensagem(String textoMensagem)
-        {
-            this.TextoMensagem = textoMensagem.ToUpper();
-        }
+
     }
 }
--- 02.Nivelamento Prático/Curso de C#/repos/pooMensagem/pooMensagem/Program.cs
+++ 02.Nivelamento Prático/Curso de C#/repos/pooMensagem/pooMensagem/Program.cs
@@ -13,20 +13,29 @@ namespace pooMensagem
             //Console.WriteLine("Olá mundo!");
             Mensagem msg1, msg2;
             msg1 = new Mensagem();
+            msg1 = new Mensagem();
             //msg1.TextoMensagem = "Olá mundo!";
+            msg1.TextoMensagem = "Olá mundo!";
+            msg1.ExibirMensagem();
+
 
             //Encapsulamento
-            msg1.setTextoMensagem("Olá mundo");
+            //msg1.setTextoMensagem("Olá mundo");
+
             //Console.WriteLine(msg1.TextoMensagem);
             //msg1.ExibirMensagem();
-            Console.WriteLine(msg1.getTextoMensagem());
+            //Console.WriteLine(msg1.getTextoMensagem());
 
-            msg2  = new Mensagem();
-            //Encapsulamento
+            msg2 = new Mensagem();
+
             //msg2.TextoMensagem = "Segundo objeto";
-            msg2.setTextoMensagem("Segundo objeto");
+            msg2.TextoMensagem = "Segundo objeto";
+            msg2.ExibirMensagem();
+
+            //Encapsulamento
+            //msg2.setTextoMensagem("Segundo objeto");
             //msg2.ExibirMensagem();
-            Console.WriteLine(msg2.getTextoMensagem());
+            //Console.WriteLine(msg2.getTextoMensagem());
             Console.ReadKey();
         }
     }
