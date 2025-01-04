d1002b09a5f9a4d8ac0b764deb4d18b43ddc3799
emersonpessoa01 <eps011108@gmail.com>
2025-01-04 17:13:35 -0300

    feat: Added AreaRetangulo :rocket:

---
+++ 02.Nivelamento Prático/Curso de C#/repos/AreaRetangulo/AreaRetangulo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaRetangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+            Console.WriteLine("Calcula a área do retângulo: ");
+            Console.WriteLine("Informe a altura: ");
+            retangulo.Altura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Informe a base: ");
+            retangulo.Base = Convert.ToDouble(Console.ReadLine());
+            retangulo.ExibirDados();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 02.Nivelamento Prático/Curso de C#/repos/AreaRetangulo/AreaRetangulo/Retangulo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaRetangulo
+{
+    internal class Retangulo
+
+    {
+       
+        public Retangulo()
+        {
+            this.Altura = 0;
+            this.Base = 0;
+        }
+
+        //Base do retângulo
+        private double baseret;
+
+        public double Base
+        {
+            get { return baseret; }
+            set
+            {
+                if (value >= 0)
+                    baseret = value;
+                else baseret = 0;
+            }
+        }
+        //Altura do retângulo
+        private double altret;
+        public double Altura
+        {
+            get { return altret; }
+            set
+            {
+                if (value >= 0)
+                    altret = value;
+                else altret = 0;
+            }
+        }
+
+        public double Area
+        {
+            get
+            {
+                return baseret * altret;
+            }
+        }
+
+        public void ExibirDados()
+        {
+            Console.WriteLine("Base: "+ this.Base);
+            Console.WriteLine("Altura: "+ this.Altura);
+            Console.WriteLine("Area: "+ this.Area);
+        }
+
+    }
+}
