635b839b7891c06d1912370c5e26cf50a227e2fc
emersonpessoa01 <eps011108@gmail.com>
2025-01-04 17:48:18 -0300

    feat: Added pooPokemon :rocket:

---
+++ 02.Nivelamento Prático/Curso de C#/repos/pooPokemon/pooPokemon/Pokemon.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooPokemon
+{
+    public class Pokemon
+    {
+        public Pokemon() {
+            this.Nome = ""; //Nome vai começarem branco
+            this.Descricao = ""; //Descrição vai começarem branco
+        }
+        private String nome;
+
+        public String Nome
+        {
+            get
+            {
+                return nome;
+            }
+            set
+            {
+                String texto = value.ToUpper();
+                nome = texto;
+            }
+        }
+
+        private String descricao; //Armazena o valor da propriedade Descricao
+
+        public String Descricao //Representa a característica Descricao do meu Pokemon
+        {
+            get { return this.descricao; }
+            set
+            {
+                String texto = value.ToUpper();
+                this.descricao = texto;
+            }
+        }
+
+        public void ExibirDaddos()
+        {
+            Console.WriteLine("Nome do Pokemon: "+ this.Nome);
+            Console.WriteLine("Descrição do Pokemon: "+this.Descricao);
+        }
+
+    }
+}
---
+++ 02.Nivelamento Prático/Curso de C#/repos/pooPokemon/pooPokemon/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooPokemon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pokemon pokemon = new Pokemon();
+            pokemon.Nome = "Bulbasauro";
+            pokemon.Descricao = "Bulbasauro é um pokemon tipo planta...";
+            pokemon.ExibirDaddos();
+            Console.ReadKey();
+        }
+    }
+}
