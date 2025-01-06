f713282054e948b7377c94981ff10ef2870e29ab
emersonpessoa01 <eps011108@gmail.com>
2025-01-06 14:12:03 -0300

    feat: Added exemploENUm :tada:

---
+++ 02.Nivelamento Prático/Curso de C#/repos/exemploENUM/exemploENUM/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemploENUM
+{
+    internal class Program
+    {
+        enum mes
+        {
+            Janeiro,
+            Fevereiro,
+            Março,
+            Abril,
+            Maio,
+            Junho,
+            Julho,
+            Agosto,
+            Setembro,
+            Outubro,
+            Novembro,
+            Dezembro
+        };
+        static void Main(string[] args)
+        {
+            //mes aniversario = mes.Janeiro;
+            mes aniversario = (mes)0;
+            Console.WriteLine("O mês do meu aniversário é no mês: " + aniversario);
+            Console.ReadKey();
+        }
+    }
+}
