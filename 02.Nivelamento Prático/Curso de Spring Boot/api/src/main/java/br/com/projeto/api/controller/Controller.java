41f9086bc791e71019215a69535d9fc55f000184
emersonpessoa01 <eps011108@gmail.com>
2025-01-31 19:06:29 -0300

    feat: Added GetMapping :sparkles:

--- 02.Nivelamento Prático/Curso de Spring Boot/api/src/main/java/br/com/projeto/api/controller/Controller.java
+++ 02.Nivelamento Prático/Curso de Spring Boot/api/src/main/java/br/com/projeto/api/controller/Controller.java
@@ -1,13 +1,26 @@
 package br.com.projeto.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-    
+
     @GetMapping("/")
     public String hello() {
         return "Hello World!";
     }
+
+    @GetMapping("/jungle")
+    public String helloJungle() {
+        /* Conacatenar nome */
+        return "Welcome to the jungle";
+    }
+    @GetMapping("/jungle/{nome}")
+    public String helloJungle(@PathVariable String nome) {
+        /* Conacatenar nome */
+        return "Welcome to the jungle, " + nome + "!";
+    }
+
 }
