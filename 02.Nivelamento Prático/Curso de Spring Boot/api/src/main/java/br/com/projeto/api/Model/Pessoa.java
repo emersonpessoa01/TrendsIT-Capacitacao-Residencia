c56107bc64b4b74fe11794def4c154776ca5ff2e
emersonpessoa01 <eps011108@gmail.com>
2025-02-01 22:28:14 -0300

    feat: Added Model :sparkles:

---
+++ 02.Nivelamento Prático/Curso de Spring Boot/api/src/main/java/br/com/projeto/api/Model/Pessoa.java
@@ -0,0 +1,23 @@
+package br.com.projeto.api.Model;
+
+public class Pessoa {
+    //Atributo
+    private String nome;
+    private int idade;
+    //Getters and Setters
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+
+    
+}
