9b92e43d27f13e4ce5f393a3c0a3c30555f47e6d
emersonpessoa01 <eps011108@gmail.com>
2025-02-02 20:14:22 -0300

    feat: Linking the Person Class with the Controller :sparkles:

--- 02.Nivelamento Prático/Curso de Spring Boot/api/src/main/java/br/com/projeto/api/controller/Controller.java
+++ 02.Nivelamento Prático/Curso de Spring Boot/api/src/main/java/br/com/projeto/api/controller/Controller.java
@@ -2,7 +2,11 @@ package br.com.projeto.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.projeto.api.Model.Pessoa;
 
 @RestController
 public class Controller {
@@ -22,5 +26,10 @@ public class Controller {
         /* Conacatenar nome */
         return "Welcome to the jungle, " + nome + "!";
     }
+    /* Vincular classe Pessoa com a Controller */
+    @PostMapping("/pessoa") //Responsável por exibir um nomee idade
+    public Pessoa pessoa(@RequestBody Pessoa p){
+       return p;
+    } 
 
 }
