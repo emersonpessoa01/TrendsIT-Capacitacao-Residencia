3abf3f57d126cf9c3f3a6f12dfb0ec5576dde197
emersonpessoa01 <eps011108@gmail.com>
2025-02-06 21:10:47 -0300

    feat:Added findById :sparkles:

--- 02.Nivelamento Prático/Curso de Spring Boot/api/src/main/java/br/com/projeto/api/controller/Controller.java
+++ 02.Nivelamento Prático/Curso de Spring Boot/api/src/main/java/br/com/projeto/api/controller/Controller.java
@@ -13,35 +13,43 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.projeto.api.Model.Pessoa;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @RestController
 public class Controller {
     @Autowired // injeção de dependência que precisa mais instanciar métodos
-    private Repository repository; //Irá conter as ações do banco de dados:cadastrar, deletar, atualizar e listar 
+    private Repository repository; // Irá conter as ações do banco de dados:cadastrar, deletar, atualizar e listar
 
-    //Método para rota de cadastro
+    // Método para rota de cadastro
     @PostMapping("/api")
     public Pessoa cadastrar(@RequestBody Pessoa pessoa) {
         return repository.save(pessoa);
     }
-    
-    @GetMapping("/")
-    public String hello() {
-        return "Hello World!";
-    }
+    // Método para rota de listagem
     @GetMapping("/api")
     public List<Pessoa> selecionar() {
         return repository.findAll();
     }
+
+    // Método para rota de listar por id
+    @GetMapping("/api/{codigo}")
+    public Pessoa selecionarPeloCodigo(@PathVariable int codigo) {
+        return repository.findById(codigo).get();
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World!";
+    }
+
     public String getMethodName(@RequestParam String param) {
         return new String();
     }
-    
+
     @GetMapping("/welcome")
     public String helloJungle() {
         /* Concatenar nome */
         return "Welcome to the jungle";
     }
+
     @GetMapping("/welcome/{nome}")
     public String helloJungle(@PathVariable String nome) {
         /* Concatenar nome */
@@ -50,8 +58,7 @@ public class Controller {
     /* Vincular classe Pessoa com a Controller */
     // @PostMapping("/api/pessoa") //Responsável por exibir um nome e idade
     // public Pessoa pessoa(@RequestBody Pessoa p){
-    //    return p;
-    // } 
-    
+    // return p;
+    // }
 
 }
