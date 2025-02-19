055b89782a6ea7b299861be1b0902909b77f2824
emersonpessoa01 <eps011108@gmail.com>
2025-02-18 23:01:29 -0300

    feat: ResponseEntityExceptionHandler :sparkles:

--- 02.Nivelamento Prático/Curso de Spring Boot/api/src/main/java/br/com/projeto/api/Model/Cliente.java
+++ 02.Nivelamento Prático/Curso de Spring Boot/api/src/main/java/br/com/projeto/api/Model/Cliente.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 
 @Entity
@@ -17,7 +18,7 @@ public class Cliente {
     @NotEmpty(message = "Informe um nome")//Garante que o campo não seja nulo
     private String nome;
 
-    @NotEmpty(message = "Informe um e-mail válido") //Validação de email se tem @ e .
+    @Email(message = "Informe um e-mail válido") //Validação de email se tem @ e .
     private String email;
 
     //Os métodos get e set
