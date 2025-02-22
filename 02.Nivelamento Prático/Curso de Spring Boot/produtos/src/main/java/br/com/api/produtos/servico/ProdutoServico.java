ac2a3212d263416618ab7534f04455bc8042df8c
emersonpessoa01 <eps011108@gmail.com>
2025-02-22 10:05:24 -0300

    feat: Product listing service :sparkles:

--- 02.Nivelamento Prático/Curso de Spring Boot/produtos/src/main/java/br/com/api/produtos/servico/ProdutoServico.java
+++ 02.Nivelamento Prático/Curso de Spring Boot/produtos/src/main/java/br/com/api/produtos/servico/ProdutoServico.java
@@ -1,5 +1,19 @@
 package br.com.api.produtos.servico;
 
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.api.produtos.modelo.ProdutoModelo;
+import br.com.api.produtos.repositorio.ProdutoRepositorio;
+
+@Service
 public class ProdutoServico {
-    
+    // Implementações dos métodos do ProdutoServico
+    @Autowired
+    private ProdutoRepositorio produtoRepositorio;
+
+    // Método para listar todos os produtos
+    public Iterable<ProdutoModelo> listar() {
+        return produtoRepositorio.findAll();
+    }
 }
