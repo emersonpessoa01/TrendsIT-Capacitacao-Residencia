2de408153a1d000c18bea0f299fe7995ece5c64d
emersonpessoa01 <eps011108@gmail.com>
2025-02-23 20:54:54 -0300

    feat: Service for registering products :sparkles:

--- 02.Nivelamento Prático/Curso de Spring Boot/produtos/src/main/java/br/com/api/produtos/servico/ProdutoServico.java
+++ 02.Nivelamento Prático/Curso de Spring Boot/produtos/src/main/java/br/com/api/produtos/servico/ProdutoServico.java
@@ -2,8 +2,11 @@ package br.com.api.produtos.servico;
 
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import br.com.api.produtos.modelo.ProdutoModelo;
+import br.com.api.produtos.modelo.RespostaModelo;
 import br.com.api.produtos.repositorio.ProdutoRepositorio;
 
 @Service
@@ -12,10 +15,27 @@ public class ProdutoServico {
     @Autowired
     private ProdutoRepositorio produtoRepositorio;
 
+    @Autowired
+    private RespostaModelo respostaModelo;
+
     // Método para listar todos os produtos
-    //Iterable é uma interface que representa uma coleção de elementos.
-    //E toda lista que tem um tipo de dado que pode ser retornado
+    // Iterable é uma interface que representa uma coleção de elementos.
+    // E toda lista que tem um tipo de dado que pode ser retornado
     public Iterable<ProdutoModelo> listar() {
         return produtoRepositorio.findAll();
+    }
+
+    // Método para salvar um produto
+    public ResponseEntity<?> cadastrar(ProdutoModelo produtoModelo) {
+        if (produtoModelo.getNome().equals("")) {
+            respostaModelo.setMensagem("O nome do produto é obrigatório!");
+            return new ResponseEntity<RespostaModelo>(respostaModelo, HttpStatus.BAD_REQUEST);
+        } else if (produtoModelo.getMarca() == 0) {
+            respostaModelo.setMensagem("A marca do produto é obrigatória!");
+            return new ResponseEntity<RespostaModelo>(respostaModelo, HttpStatus.BAD_REQUEST);
+        } else {
+            return new ResponseEntity<ProdutoModelo>(produtoRepositorio.save(produtoModelo), HttpStatus.CREATED);
+        }
+        
     }
 }
