f9326e35d8c851fb603a274983ba9499771f6fbb
emersonpessoa01 <eps011108@gmail.com>
2025-02-26 12:19:04 -0300

    feat: Added CORS :sparkles:

--- 02.Nivelamento Prático/Curso de Spring Boot/produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ 02.Nivelamento Prático/Curso de Spring Boot/produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
+// @CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*") // LLiberar acesso de qualquer origem
 public class ProdutoControle {
 
     @Autowired
