1786ddb7fb5ca8083c3d7b053a9e79408441c830
emersonpessoa01 <eps011108@gmail.com>
2025-02-26 10:30:08 -0300

    feat: Route to remove products :sparkles:

--- 02.Nivelamento Prático/Curso de Spring Boot/produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ 02.Nivelamento Prático/Curso de Spring Boot/produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -3,11 +3,14 @@ package br.com.api.produtos.controle;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api.produtos.modelo.ProdutoModelo;
+import br.com.api.produtos.modelo.RespostaModelo;
 import br.com.api.produtos.servico.ProdutoServico;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,5 +35,9 @@ public class ProdutoControle {
     @PutMapping("/editar")
     public ResponseEntity<?> editar(@RequestBody ProdutoModelo produtoModelo){
         return produtoServico.cadastrarAlterar(produtoModelo, "editar");
+    }
+    @DeleteMapping("/remover/{codigo}")
+    public ResponseEntity<RespostaModelo> remover(@PathVariable Long codigo){
+        return produtoServico.remover(codigo);
     }
 }
