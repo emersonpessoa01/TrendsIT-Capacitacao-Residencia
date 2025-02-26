c5da0283ec7fa642fde021f16786117e7e9a6283
emersonpessoa01 <eps011108@gmail.com>
2025-02-26 10:52:25 -0300

    feat: Updated method of registering on the controller :sparkles:

--- 02.Nivelamento Prático/Curso de Spring Boot/produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ 02.Nivelamento Prático/Curso de Spring Boot/produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -6,6 +6,8 @@ import br.com.api.produtos.modelo.ProdutoModelo;
 import br.com.api.produtos.modelo.RespostaModelo;
 import br.com.api.produtos.servico.ProdutoServico;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,24 +22,36 @@ public class ProdutoControle {
 
     @Autowired
     private ProdutoServico produtoServico;
+
     @GetMapping("/listar")
     public Iterable<ProdutoModelo> listar() {
         return produtoServico.listar();
     }
+
     @GetMapping("/")
     public String rota() {
         return "API de produtos funcionando!";
     }
+
+    // @PostMapping("/cadastrar")
+    // public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo produtoModelo){
+    // return produtoServico.cadastrarAlterar(produtoModelo, "cadastrar");
+    // }
     @PostMapping("/cadastrar")
-    public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo produtoModelo){
-        return produtoServico.cadastrarAlterar(produtoModelo, "cadastrar");
+    public ResponseEntity<?> cadastrar(@RequestBody List<ProdutoModelo> produtoModelo) {
+        for (ProdutoModelo produto : produtoModelo) {
+            produtoServico.cadastrarAlterar(produto, "cadastrar");
+        }
+        return ResponseEntity.ok("Produtos cadastrados com sucesso!");
     }
+
     @PutMapping("/editar")
-    public ResponseEntity<?> editar(@RequestBody ProdutoModelo produtoModelo){
+    public ResponseEntity<?> editar(@RequestBody ProdutoModelo produtoModelo) {
         return produtoServico.cadastrarAlterar(produtoModelo, "editar");
     }
+
     @DeleteMapping("/remover/{codigo}")
-    public ResponseEntity<RespostaModelo> remover(@PathVariable Long codigo){
+    public ResponseEntity<RespostaModelo> remover(@PathVariable Long codigo) {
         return produtoServico.remover(codigo);
     }
 }
