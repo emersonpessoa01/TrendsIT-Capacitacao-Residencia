b790163986d0abb1decd0859e010744ac62c1a36
emersonpessoa01 <eps011108@gmail.com>
2025-02-27 11:43:18 -0300

    feat: Form structure :sparkles:

--- 02.Nivelamento Prático/Curso de Spring Boot/frontend/src/components/Formulario.jsx
+++ 02.Nivelamento Prático/Curso de Spring Boot/frontend/src/components/Formulario.jsx
@@ -2,9 +2,14 @@ import React from "react";
 
 const Formulario = () => {
   return (
-    <div>
-      <h1>Formulário</h1>
-    </div>
+    <form>
+      <input type="text" placeholder="Nome do produto" />
+      <input type="text" placeholder="Marca" />
+      <input type="button" value="Cadastrar" />
+      <input type="button" value="Alterar" />
+      <input type="button" value="Remover" />
+      <input type="button" value="Cancelar" />
+    </form>
   );
 };
 
