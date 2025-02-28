99db92b803a8fce7a9697624a47a390d73430426
emersonpessoa01 <eps011108@gmail.com>
2025-02-28 12:42:33 -0300

    feat: Button visibility :sparkles:

--- 02.Nivelamento Prático/Curso de Spring Boot/frontend/src/App.jsx
+++ 02.Nivelamento Prático/Curso de Spring Boot/frontend/src/App.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import Formulario from "./components/Formulario";
 import Tabela from "./components/Tabela";
 import "./App.css";
 
 function App() {
+  const [btnCadastrar, setbtnCadastrar] = useState(true);
+  
   return (
     <div>
       <h1>Produtos</h1>
-      <Formulario />
+      <Formulario botao={btnCadastrar} />
       <Tabela />
     </div>
   );
--- 02.Nivelamento Prático/Curso de Spring Boot/frontend/src/components/Formulario.jsx
+++ 02.Nivelamento Prático/Curso de Spring Boot/frontend/src/components/Formulario.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 
-const Formulario = () => {
+const Formulario = ({ botao }) => {
   return (
     <form>
       <input autoFocus type="text" placeholder="Nome do produto" className="form-control" />
-      <input type="text" placeholder="Marca" className="form-control"/>
-      <input type="button" value="Cadastrar" className="btn btn-primary"/>
-      <input type="button" value="Alterar" className="btn btn-warning"/>
-      <input type="button" value="Remover" className="btn btn-danger"/>
-      <input type="button" value="Cancelar"className="btn btn-secondary" />
+      <input type="text" placeholder="Marca" className="form-control" />
+      {botao ? (
+        <input type="button" value="Cadastrar" className="btn btn-primary" />
+      ) : (
+        <div>
+          <input type="button" value="Alterar" className="btn btn-warning" />
+          <input type="button" value="Remover" className="btn btn-danger" />
+          <input type="button" value="Cancelar" className="btn btn-secondary" />
+        </div>
+      )}
     </form>
   );
 };
