ff8170767db326c29ea770ab5a1aea4b041026f4
emersonpessoa01 <eps011108@gmail.com>
2025-03-01 10:34:43 -0300

    feat: Getting the products :sparkles:

--- 02.Nivelamento Prático/Curso de Spring Boot/frontend/src/App.jsx
+++ 02.Nivelamento Prático/Curso de Spring Boot/frontend/src/App.jsx
@@ -1,14 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Formulario from "./components/Formulario";
 import Tabela from "./components/Tabela";
 import "./App.css";
 
 function App() {
-  const [btnCadastrar, setbtnCadastrar] = useState(true);
-  
+  const [btnCadastrar, setBtnCadastrar] = useState(true);
+  const [produtos, setProdutos] = useState([]);
+
+  useEffect(() => {
+    // fetch("http://localhost:8081/listar")
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     setpProdutos(data);
+    //   });
+    //Usar async await
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:8081/listar");
+      const data = await response.json();
+      setProdutos(data);
+      console.log(data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
       <h1>Produtos</h1>
+      {/* <p>{JSON.stringify(produtos)}</p> */}
       <Formulario botao={btnCadastrar} />
       <Tabela />
     </div>
