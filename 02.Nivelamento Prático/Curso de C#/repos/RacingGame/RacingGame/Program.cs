daa514c9f54ed8b0702d690d067df8210d89b675
emersonpessoa01 <eps011108@gmail.com>
2025-03-22 16:41:51 -0300

    feat: Telling the runner to run :rocket:

--- 02.Nivelamento Prático/Curso de C#/repos/RacingGame/RacingGame/Program.cs
+++ 02.Nivelamento Prático/Curso de C#/repos/RacingGame/RacingGame/Program.cs
@@ -16,28 +16,33 @@ namespace RacingGame
             c1.Nome = "e";
             String pista = ""; //Vai representar a pista
             string corredorPista = ""; //Vai representar o corredor na pista
-            posicaoC1 = c1.Correr(); //Chama o método correr da classe corredor
 
-
-            //Montar a imagem do corredor na pista
-            pista = ""; //Inicia a pista vazia
-            corredorPista = ""; //Inicia o corredor na pista vazia
-            for (int i = 0; i <= tlPista; i++)
+            //Enquanto a posição do corredor for menor que o tamanho da pista(Está mandando o corredor correr)
+            posicaoC1 = 0;
+            while (posicaoC1 < tlPista)
             {
-                pista += "_"; //Monta a pista
-                if (i == posicaoC1)
+
+            posicaoC1 += c1.Correr(); //A posição do corredor é incrementada com o valor retornado pelo método correr
+                                      //Montar a imagem do corredor na pista
+                pista = ""; //Inicia a pista vazia
+            corredorPista = ""; //Inicia o corredor na pista vazia
+                for (int i = 0; i <= tlPista; i++)
                 {
-                    corredorPista += c1.Nome; //Monta o corredor na pista
+                    pista += "_"; //Monta a pista
+                    if (i == posicaoC1)
+                    {
+                        corredorPista += c1.Nome; //Monta o corredor na pista
+                    }
+                    else
+                    {
+                        corredorPista += " "; //Monta o corredor na pista
+                    }
                 }
-                else
-                {
-                    corredorPista += " "; //Monta o corredor na pista
-                }
-            }
             Console.WriteLine(pista); //Imprime a pista
             Console.WriteLine(corredorPista); //Imprime o corredor na pista
             //Console.WriteLine("Nome: " + c1.Nome);
             Console.ReadKey();
+            }
 
         }
     }
